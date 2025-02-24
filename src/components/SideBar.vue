e619fbfe93d2a2b662914d938ff3cc8a2681602e
John Wei <wzy1048168235@163.com>
2025-02-25 01:21:18 +0800

    feat(main): 构建Course列表

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,43 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Icon } from "@iconify/vue";
-import { useRouter } from "vue-router";
+import CourseItem from "@/components/CourseItem.vue";
+import Course from "@/model/Course.ts";
 
-const router = useRouter();
-const selected = ref(0)
+const items = ref([
+  new Course('test1', '测试1'),
+  new Course('test2', '测试2'),
+  new Course('test3', '测试3'),
+  new Course('test4', '测试4'),
+])
 
-function onSelected(index: number) {
-  selected.value = index
-  if (index === 0) {
-    router.push("/")
-  } else if (index === 1) {
-    router.push("/course")
-  }
+function onItemClick(item: Course) {
+  alert(item.id)
 }
 
 </script>
 
 <template>
-  <div class="flex flex-col items-center bg-gray-200 text-black w-16">
-    <button
-      class="mb-2 mt-2 rounded p-2 group"
-      :class="{
-        'bg-blue-500': selected === 0,
-        'hover:bg-gray-300': selected !== 0
-      }"
-      @click="onSelected(0)">
-      <Icon icon="ic:baseline-schedule" width="24" height="24" :class="selected === 0 ? 'text-white' : 'text-black'"/>
-    </button>
-    <button
-      class="mt-2 rounded p-2 group"
-      :class="{
-        'bg-blue-500': selected === 1,
-        'hover:bg-gray-300': selected !== 1,
-      }"
-      @click="onSelected(1)">
-      <Icon icon="ic:baseline-format-list-bulleted" width="24" height="24"
-            :class="selected === 1 ? 'text-white' : 'text-black'"/>
-    </button>
+  <div class="container flex flex-col bg-gray-200 text-black w-1/5 p-2 gap-2">
+    <CourseItem :item="item" @click="onItemClick" v-for="item in items" :key="item.id" class="w-full"/>
   </div>
 </template>
 
---
+++ src/model/Course.ts
@@ -0,0 +1,16 @@
+import { v4 as uuid } from "uuid";
+import randomColor from "randomcolor";
+
+export default class Course {
+  id: string
+  student: string
+  content: string
+  color: string
+
+  constructor(student: string, content: string) {
+    this.id = uuid()
+    this.student = student
+    this.content = content
+    this.color = randomColor()
+  }
+}
